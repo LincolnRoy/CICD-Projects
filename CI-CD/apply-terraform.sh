41b25145e8f01137f72b6ecbfcfd8a1695a6655e
Lincoln Roy <roylincoln002@gmail.com>
2023-07-19 14:23:27 -0400

    destroy

--- CI-CD/apply-terraform.sh
+++ CI-CD/apply-terraform.sh
@@ -7,10 +7,10 @@ set -eu
 cd .. 
 
 # initialize terraform
- terraform init
+terraform init
 
 # # apply terraform
- terraform apply -auto-approve
+# terraform apply -auto-approve
 
 # destroy terraform
-# terraform destroy -auto-approve
+terraform destroy -auto-approve
